e5a0a071811078a58faba3081e557ed03c5db9ed
alex <alexsolomyaniy@gmail.com>
2019-09-07 13:38:09 +0300

    Test

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'question.dart';
 
 /*void main(){
 runApp(MyApp());
@@ -6,16 +7,48 @@ runApp(MyApp());
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+
+@override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+
+  }
+}
+
+class _MyAppState extends State<MyApp>{
+
+  var _index=0;
+
+  void _answerQuestion() {
+    setState(() {
+      _index=_index+1;
+    });
+    print(_index);
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = ['What is it', 'WHo is it'];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('App'),
         ),
-        body: Text('My default text'),
+        body: Column(children: [
+          Text(questions[_index]),
+          RaisedButton(
+            child: Text('Hello'),
+            onPressed: _answerQuestion,
+          ),
+          RaisedButton(
+            child: Text('Hello'),
+            onPressed: () => print('Hop'),
+          )
+        ]),
       ),
     );
   }
 }
+
