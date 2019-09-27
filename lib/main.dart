4be0d8d4ff818ea771eb38c30cef33a0fc6d818c
AlexSolomyaniy <alexsolomyaniy@gmail.com>
2019-09-27 16:22:17 +0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -9,40 +9,52 @@ runApp(MyApp());
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
-
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
     return _MyAppState();
-
   }
 }
 
-class _MyAppState extends State<MyApp>{
-
-  var _index=0;
+class _MyAppState extends State<MyApp> {
+  var _questioniIndex = 0;
 
   void _answerQuestion() {
     setState(() {
-      _index=_index+1;
+      _questioniIndex = _questioniIndex + 1;
     });
-    print(_index);
+    print(_questioniIndex);
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = ['What is it', 'WHo is it'];
+    var questions = [
+      {
+        'questionText': 'How old are you?',
+        'answer': [1, 2, 3, 4]
+      },
+      {
+        'questionText': 'What is your favorite color?',
+        'answer': ['green', 'yellow', 'red']
+      }
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('App'),
         ),
-        body: Column(children: [
-          Question(questions[_index],
+        body: Column(
+          children: [
+            Question(
+              questions[_questioniIndex]['questionText'],
+            ),
+            ...(questions[_questioniIndex]['answers'] as List<String>)
+                .map((answer) {
+              return Answer(_answerQuestion, answer);
+            }).toList()
+          ],
         ),
-          Answer(_answerQuestion),
-          Answer(_answerQuestion),
-        ],),
       ),
     );
   }
